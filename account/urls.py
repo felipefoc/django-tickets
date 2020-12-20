83d752bc2abe7f3e8c344b7c735736bb99970a00
Felipe <felipemfmayer@gmail.com>
2020-12-20 18:15:03 -0300

    added register page front end

--- account/urls.py
+++ account/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.homePage, name='home'),
+    path('', views.loginPage, name='login'),
+    path('login/', views.loginPage, name='login'),
+    path('registrar/', views.registerPage, name='register'),    
 ]
--- account/views.py
+++ account/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-def homePage(request):
+def loginPage(request):
     context = {}
-    return render(request, 'templates\login-page.html', context)
+    return render(request, 'templates/login-page.html', context)
+
+
+def registerPage(request):
+    context = {}
+    return render(request, 'templates/register-page.html', context)
